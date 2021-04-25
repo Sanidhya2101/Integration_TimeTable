c6726f96e98382e9d7bb2496f146f8e2b0dbdafa
Atharvat <tagalpallewaratharva@gmail.com>
2021-04-25 12:39:20 +0530

    Microsoft Auth

--- app/src/main/java/com/example/timetable_integration/MainActivity.java
+++ app/src/main/java/com/example/timetable_integration/MainActivity.java
@@ -10,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       /* public static void printHashKey(Context pContext) {
+            try {
+                PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+                for (Signature signature : info.signatures) {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    String hashKey = new String(Base64.encode(md.digest(), 0));
+                    Log.d("HashKey", "printHashKey() Hash Key: " + hashKey);
+                }
+            } catch (NoSuchAlgorithmException e) {
+                Log.e(TAG, "printHashKey()", e);
+            } catch (Exception e) {
+                Log.e(TAG, "printHashKey()", e);
+            }
+        }*/
     }
 }
