d0a3ac0b9ee7c59edbbc49609df39d7240e8b7ff
Atharvat <tagalpallewaratharva@gmail.com>
2021-05-18 16:20:32 +0530

    Add Quiz, Viva and Assignment Functions

--- app/src/main/java/com/example/timetable_integration/MainActivity.java
+++ app/src/main/java/com/example/timetable_integration/MainActivity.java
@@ -2,18 +2,27 @@ package com.example.timetable_integration;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.timetable_integration.Adaptor.CourseAdaptor;
@@ -21,6 +30,8 @@ import com.example.timetable_integration.Models.Course;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -31,7 +42,12 @@ import com.google.firebase.firestore.QuerySnapshot;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +56,11 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Course> coursedata;
     CourseAdaptor courseAdaptor;
     FirebaseFirestore fstore;
+
+    String student_program = "BTech";
+    String student_year = "First_Year";
+    String student_semester = "Semester 1";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,9 +78,6 @@ public class MainActivity extends AppCompatActivity {
 
         courses_rec.setLayoutManager(ll);
         courseAdaptor = new CourseAdaptor(coursedata,this);
-
-
-        
 
         fstore.collection("Timetable").orderBy("course_time", Query.Direction.DESCENDING)
                 .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
@@ -169,28 +187,75 @@ public class MainActivity extends AppCompatActivity {
                 // show the popup window
                 // which view you pass in doesn't matter, it is only used for the window tolken
                 popupClassWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
+
+                Map<String, Object> data = new HashMap<>();
+
+                Button time_selector = popupclassView.findViewById(R.id.new_assignment_deadline);
+                final long[] time = new long[1];
+                time_selector.setOnClickListener(view2 -> {
+                    data.put("Time", new Timestamp(showDateTimePicker(popupclassView.getContext()).getTime()));
+                });
+                Button update_time = popupclassView.findViewById(R.id.add_assignment);
+                EditText  code = popupclassView.findViewById(R.id.new_assignment_code);
+                EditText  name = popupclassView.findViewById(R.id.new_assignment_name);
+                EditText  platform = popupclassView.findViewById(R.id.new_assignment_platform);
+                update_time.setOnClickListener(v1 -> {
+                    data.put("Code",code.getText().toString());
+                    data.put("Name",name.getText().toString());
+                    data.put("Platform",platform.getText().toString());
+                    data.put("Status","Pending");
+                    fstore.collection("TimeTable").document(student_program).collection(student_year).document(student_semester).collection("Assignment").add(data);
+                    popupClassWindow.dismiss();
+                });
+
             }
         });
-        add_new_quiz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view1) {
-                popupWindow.dismiss();
-                //  new_class(view);
-                LayoutInflater inflater = (LayoutInflater)
-                        getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupclassView = inflater.inflate(R.layout.popup_new_quiz, null);
+        add_new_quiz.setOnClickListener(view1 -> {
+            Map<String, Object> data = new HashMap<>();
 
-                // create the popup window
-                int width_class = LinearLayout.LayoutParams.MATCH_PARENT;
-                int height_class = LinearLayout.LayoutParams.WRAP_CONTENT;
-                //boolean focusable = true; // lets taps outside the popup also dismiss it
-                boolean focusable = true;
-                final PopupWindow popupClassWindow = new PopupWindow(popupclassView, width_class, height_class, focusable);
+            popupWindow.dismiss();
+            //  new_class(view);
+            LayoutInflater inflater1 = (LayoutInflater)
+                    getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popupclassView = inflater1.inflate(R.layout.popup_new_quiz, null);
 
-                // show the popup window
-                // which view you pass in doesn't matter, it is only used for the window tolken
-                popupClassWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
-            }
+            // create the popup window
+            int width_class = LinearLayout.LayoutParams.MATCH_PARENT;
+            int height_class = LinearLayout.LayoutParams.WRAP_CONTENT;
+            //boolean focusable = true; // lets taps outside the popup also dismiss it
+            boolean focusable1 = true;
+            final PopupWindow popupClassWindow = new PopupWindow(popupclassView, width_class, height_class, focusable1);
+
+            // show the popup window
+            // which view you pass in doesn't matter, it is only used for the window tolken
+            popupClassWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
+
+            Button time_selector = popupclassView.findViewById(R.id.new_quiz_time_button);
+            final long[] time = new long[1];
+            time_selector.setOnClickListener(view2 -> {
+                data.put("Time", new Timestamp(showDateTimePicker(popupclassView.getContext()).getTime()));
+            });
+            Button update_time = popupclassView.findViewById(R.id.add_quiz);
+            EditText  code = popupclassView.findViewById(R.id.new_quiz_code);
+            EditText  name = popupclassView.findViewById(R.id.new_quiz_name);
+            EditText  duration = popupclassView.findViewById(R.id.new_quiz_duration);
+            EditText  platform = popupclassView.findViewById(R.id.new_quiz_platform);
+            update_time.setOnClickListener(v1 -> {
+                data.put("Code",code.getText().toString());
+                data.put("Name",name.getText().toString());
+                data.put("Duration",duration.getText().toString());
+                data.put("Platform",platform.getText().toString());
+                data.put("Status","Pending");
+                fstore.collection("TimeTable").document(student_program).collection(student_year).document(student_semester).collection("Quiz").add(data)
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull @NotNull Exception e) {
+                                Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                popupClassWindow.dismiss();
+            });
+
         });
         add_new_lab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -220,7 +285,7 @@ public class MainActivity extends AppCompatActivity {
                 //  new_class(view);
                 LayoutInflater inflater = (LayoutInflater)
                         getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupclassView = inflater.inflate(R.layout.popup_new_class, null);
+                View popupclassView = inflater.inflate(R.layout.popup_new_viva, null);
 
                 // create the popup window
                 int width_class = LinearLayout.LayoutParams.MATCH_PARENT;
@@ -232,6 +297,34 @@ public class MainActivity extends AppCompatActivity {
                 // show the popup window
                 // which view you pass in doesn't matter, it is only used for the window tolken
                 popupClassWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
+
+                Map<String, Object> data = new HashMap<>();
+
+                Button time_selector = popupclassView.findViewById(R.id.new_viva_time_button);
+                time_selector.setOnClickListener(view2 -> {
+                    data.put("Time", new Timestamp(showDateTimePicker(popupclassView.getContext()).getTime()));
+                });
+                Button update_time = popupclassView.findViewById(R.id.add_viva);
+                EditText  code = popupclassView.findViewById(R.id.new_viva_code);
+                EditText  name = popupclassView.findViewById(R.id.new_viva_name);
+                EditText  duration = popupclassView.findViewById(R.id.new_viva_duration);
+                EditText  platform = popupclassView.findViewById(R.id.new_viva_platform);
+                update_time.setOnClickListener(v1 -> {
+                    data.put("Code",code.getText().toString());
+                    data.put("Name",name.getText().toString());
+                    data.put("Duration",duration.getText().toString());
+                    data.put("Platform",platform.getText().toString());
+                    data.put("Status","Pending");
+                    fstore.collection("TimeTable").document(student_program).collection(student_year).document(student_semester).collection("Viva").add(data)
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull @NotNull Exception e) {
+                                    Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                    popupClassWindow.dismiss();
+                });
+
             }
         });
         popupView.setOnTouchListener(new View.OnTouchListener() {
@@ -378,4 +471,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    public Calendar showDateTimePicker(Context context) {
+        Calendar date;
+        date = Calendar.getInstance();
+        new DatePickerDialog(context, (view, year, monthOfYear, dayOfMonth) -> {
+            date.set(year, monthOfYear, dayOfMonth);
+            new TimePickerDialog(context, (view1, hourOfDay, minute) -> {
+                date.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                date.set(Calendar.MINUTE, minute);
+                Log.v("abcd", "The choosen one " + date.getTime());
+            }, date.get(Calendar.HOUR_OF_DAY), date.get(Calendar.MINUTE), false).show();
+        }, date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DATE)).show();
+        return date;
+    }
+
 }
