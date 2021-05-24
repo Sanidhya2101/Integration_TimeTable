275050c22c3d6be88e8d71ea101c854af52e4655
Prl-1234 <priyal0917@gmail.com>
2021-05-24 14:24:20 +0530

    updated icons and design

--- app/src/main/java/com/example/timetable_integration/MainActivity.java
+++ app/src/main/java/com/example/timetable_integration/MainActivity.java
@@ -55,7 +55,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    FloatingActionButton add_new_event,edit_event;
+    FloatingActionButton add_new_event;
     RecyclerView courses_rec;
     ArrayList<Course> coursedata;
     CourseAdaptor courseAdaptor;
@@ -75,7 +75,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         add_new_event=(FloatingActionButton) findViewById(R.id.add_new_event);
-        edit_event=(FloatingActionButton) findViewById(R.id.edit_event);
         fstore = FirebaseFirestore.getInstance();
         courses_rec = findViewById(R.id.course_class);
         coursedata = new ArrayList<>();
@@ -182,12 +181,7 @@ public class MainActivity extends AppCompatActivity {
                 new_event(view);
             }
         });
-        edit_event.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                edit_event_f(view);
-            }
-        });
+
     }
 
     private void displayToast(String s) {
@@ -347,7 +341,7 @@ public class MainActivity extends AppCompatActivity {
                 //  new_class(view);
                 LayoutInflater inflater = (LayoutInflater)
                         getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupclassView = inflater.inflate(R.layout.popup_new_class, null);
+                View popupclassView = inflater.inflate(R.layout.popup_new_lab, null);
 
                 // create the popup window
                 int width_class = LinearLayout.LayoutParams.MATCH_PARENT;
@@ -420,141 +414,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    private void edit_event_f(View view) {
-        //  add_new_event.hide();
-        LayoutInflater inflater = (LayoutInflater)
-                getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.popup_edit_events, null);
 
-        // create the popup window
-        int width = LinearLayout.LayoutParams.MATCH_PARENT;
-        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        //boolean focusable = true; // lets taps outside the popup also dismiss it
-        boolean focusable = true;
-        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
-
-        // show the popup window
-        // which view you pass in doesn't matter, it is only used for the window tolken
-        popupWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
-        RelativeLayout edit_class=(RelativeLayout) popupView.findViewById(R.id.select_class);
-        RelativeLayout edit_assignment=(RelativeLayout) popupView.findViewById(R.id.select_assignment);
-        RelativeLayout edit_quiz=(RelativeLayout) popupView.findViewById(R.id.select_quiz);
-        RelativeLayout edit_lab=(RelativeLayout) popupView.findViewById(R.id.select_lab);
-        RelativeLayout edit_viva=(RelativeLayout) popupView.findViewById(R.id.select_viva);
-        edit_class.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view1) {
-                popupWindow.dismiss();
-                //  new_class(view);
-                LayoutInflater inflater = (LayoutInflater)
-                        getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupclassView = inflater.inflate(R.layout.popup_edit_class, null);
-
-                // create the popup window
-                int width_class = LinearLayout.LayoutParams.MATCH_PARENT;
-                int height_class = LinearLayout.LayoutParams.WRAP_CONTENT;
-                //boolean focusable = true; // lets taps outside the popup also dismiss it
-                boolean focusable = true;
-                final PopupWindow popupClassWindow = new PopupWindow(popupclassView, width_class, height_class, focusable);
-
-                // show the popup window
-                // which view you pass in doesn't matter, it is only used for the window tolken
-                popupClassWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
-            }
-        });
-        edit_assignment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view1) {
-                popupWindow.dismiss();
-                //  new_class(view);
-                LayoutInflater inflater = (LayoutInflater)
-                        getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupclassView = inflater.inflate(R.layout.popup_edit_assignment, null);
-
-                // create the popup window
-                int width_class = LinearLayout.LayoutParams.MATCH_PARENT;
-                int height_class = LinearLayout.LayoutParams.WRAP_CONTENT;
-                //boolean focusable = true; // lets taps outside the popup also dismiss it
-                boolean focusable = true;
-                final PopupWindow popupClassWindow = new PopupWindow(popupclassView, width_class, height_class, focusable);
-
-                // show the popup window
-                // which view you pass in doesn't matter, it is only used for the window tolken
-                popupClassWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
-            }
-        });
-        edit_quiz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view1) {
-                popupWindow.dismiss();
-                //  new_class(view);
-                LayoutInflater inflater = (LayoutInflater)
-                        getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupclassView = inflater.inflate(R.layout.popup_edit_quiz, null);
-
-                // create the popup window
-                int width_class = LinearLayout.LayoutParams.MATCH_PARENT;
-                int height_class = LinearLayout.LayoutParams.WRAP_CONTENT;
-                //boolean focusable = true; // lets taps outside the popup also dismiss it
-                boolean focusable = true;
-                final PopupWindow popupClassWindow = new PopupWindow(popupclassView, width_class, height_class, focusable);
-
-                // show the popup window
-                // which view you pass in doesn't matter, it is only used for the window tolken
-                popupClassWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
-            }
-        });
-        edit_lab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view1) {
-                popupWindow.dismiss();
-                //  new_class(view);
-                LayoutInflater inflater = (LayoutInflater)
-                        getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupclassView = inflater.inflate(R.layout.popup_edit_class, null);
-
-                // create the popup window
-                int width_class = LinearLayout.LayoutParams.MATCH_PARENT;
-                int height_class = LinearLayout.LayoutParams.WRAP_CONTENT;
-                //boolean focusable = true; // lets taps outside the popup also dismiss it
-                boolean focusable = true;
-                final PopupWindow popupClassWindow = new PopupWindow(popupclassView, width_class, height_class, focusable);
-
-                // show the popup window
-                // which view you pass in doesn't matter, it is only used for the window tolken
-                popupClassWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
-            }
-        });
-        edit_viva.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view1) {
-                popupWindow.dismiss();
-                //  new_class(view);
-                LayoutInflater inflater = (LayoutInflater)
-                        getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupclassView = inflater.inflate(R.layout.popup_edit_class, null);
-
-                // create the popup window
-                int width_class = LinearLayout.LayoutParams.MATCH_PARENT;
-                int height_class = LinearLayout.LayoutParams.WRAP_CONTENT;
-                //boolean focusable = true; // lets taps outside the popup also dismiss it
-                boolean focusable = true;
-                final PopupWindow popupClassWindow = new PopupWindow(popupclassView, width_class, height_class, focusable);
-
-                // show the popup window
-                // which view you pass in doesn't matter, it is only used for the window tolken
-                popupClassWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
-            }
-        });
-        popupView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                popupWindow.dismiss();
-                return true;
-            }
-        });
-
-    }
 
     public void showDateTimePicker(Context context) {
         Calendar calendar;
